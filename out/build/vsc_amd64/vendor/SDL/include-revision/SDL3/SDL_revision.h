d7729bd9053302eecba30cf5e39185cb0725d689
Sohlson <159041021+Sohlson@users.noreply.github.com>
2025-08-13 15:49:15 -0500

    Fixed gitignore and set SDL to release branch

--- out/build/vsc_amd64/vendor/SDL/include-revision/SDL3/SDL_revision.h
+++ out/build/vsc_amd64/vendor/SDL/include-revision/SDL3/SDL_revision.h
@@ -31,9 +31,9 @@
 /* #undef SDL_VENDOR_INFO */
 
 #ifdef SDL_VENDOR_INFO
-#define SDL_REVISION "SDL3-3.3.0-release-3.2.6-939-g62b9ac3a0 (" SDL_VENDOR_INFO ")"
+#define SDL_REVISION "SDL3-3.2.21-release-3.2.20-23-gc3828bb0a (" SDL_VENDOR_INFO ")"
 #else
-#define SDL_REVISION "SDL3-3.3.0-release-3.2.6-939-g62b9ac3a0"
+#define SDL_REVISION "SDL3-3.2.21-release-3.2.20-23-gc3828bb0a"
 #endif
 
 #endif /* SDL_revision_h_ */
--- src/emulator/timer.c
+++ src/emulator/timer.c
@@ -1,5 +1,5 @@
 #include "timer.h"
-#include <stdio.h>
+
 
 /*
 System clock function
